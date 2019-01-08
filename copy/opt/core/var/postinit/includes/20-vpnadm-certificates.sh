44e3ad1a2843f5e202df9db2c1ff86fa5cebf2b0
Thomas Merkel <tm@core.io>
2019-01-08 15:51:03 +0100

    Fix OpenVPN certificate generation, and path creation

--- copy/opt/core/var/postinit/includes/20-vpnadm-certificates.sh
+++ copy/opt/core/var/postinit/includes/20-vpnadm-certificates.sh
@@ -2,18 +2,24 @@
 # Generate CA, DH, ServerCert, TA for OpenVPN Server
 
 VPNADM_PATH='/opt/vpnadm'
-OPENVPN_PATH=$(sed -n "s/OPENVPN_PATH.*=.*'\(.*\)'.*/\1/p" ${VPNADM_PATH}/vpnadm_web/settings.py)
+OPENVPN_PATH=$(sed -n "s/OPENVPN_PATH.*=.*['|\"]\(.*\)['|\"].*/\1/p" ${VPNADM_PATH}/vpnadm_web/settings.py)
+
+# Create certificate folder
+mkdir -p "${OPENVPN_PATH}/certs"
 
 # Check if file exists and run generation command if required
 [ ! -f ${OPENVPN_PATH}/certs/ca.crt ] && \
-	${VPNADM_PATH}/manage.py generate_ca \
+	${VPNADM_PATH}/manage.py generate_ca > \
 	${OPENVPN_PATH}/certs/ca.crt
 [ ! -f ${OPENVPN_PATH}/certs/dh.pem ] && \
-	${VPNADM_PATH}/manage.py generate_dh \
+	${VPNADM_PATH}/manage.py generate_dh > \
 	${OPENVPN_PATH}/certs/dh.pem
 [ ! -f ${OPENVPN_PATH}/certs/srv.key ] && \
 	${VPNADM_PATH}/manage.py generate_server_cert \
 	${OPENVPN_PATH}/certs/srv.key ${OPENVPN_PATH}/certs/srv.crt
 [ ! -f ${OPENVPN_PATH}/certs/tls_auth.key ] && \
-	${VPNADM_PATH}/manage.py generate_ta \
+	${VPNADM_PATH}/manage.py generate_ta > \
 	${OPENVPN_PATH}/certs/tls_auth.key
+
+# Update permissions
+chmod 440 "${OPENVPN_PATH}/certs/*"
